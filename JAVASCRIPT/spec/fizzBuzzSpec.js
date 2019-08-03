1d62e0abbeb6fc871deed49d1c6f622deb5efe88
joseph townshend <joetownshend@mac.com>
2019-08-03 18:25:40 +0100

    feat: multiples of 15 working with tests

---
+++ JAVASCRIPT/spec/fizzBuzzSpec.js
@@ -0,0 +1,20 @@
+describe('FizzBuzz', function() {
+
+  var fizzBuzz;
+
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
+
+
+  describe('multiples of 3 and 5', function() {
+    it('Fizzbuzzes for 15', function() {
+      expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
+    });
+    it('Fizzbuzzes for 30', function() {
+      expect(fizzBuzz.play(30)).toEqual('FizzBuzz');
+    });
+  });
+
+
+});
---
+++ JAVASCRIPT/src/fizzBuzz.js
@@ -0,0 +1,8 @@
+function FizzBuzz() {
+
+};
+
+FizzBuzz.prototype.play = function(number) {
+  if (number % 15 === 0);
+  return "FizzBuzz"
+};
