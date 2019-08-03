c68f5c3f8288a5fcb01d9f584553ae36ed3430b0
joseph townshend <joetownshend@mac.com>
2019-08-03 17:26:24 +0100

    feat: multiples of 5 working with test passing

--- RUBY/lib/fizzbuzz.rb
+++ RUBY/lib/fizzbuzz.rb
@@ -5,6 +5,9 @@ def fizzbuzzer(user_input)
     return "FizzBuzz"
   elsif user_input % 3 == 0
     return "Fizz"
+  elsif user_input % 5 == 0
+    return "Buzz"
+
   end
 
 end
--- RUBY/spec/fizzbuzz_spec.rb
+++ RUBY/spec/fizzbuzz_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe FizzBuzz do
       expect(subject.fizzbuzzer(3)).to eq "Fizz"
       expect(subject.fizzbuzzer(9)).to eq "Fizz"
     end
+    it 'can return Buzz if multiple of 5' do
+      expect(subject.fizzbuzzer(5)).to eq "Buzz"
+      expect(subject.fizzbuzzer(10)).to eq "Buzz"
+    end
   end
 
 end
