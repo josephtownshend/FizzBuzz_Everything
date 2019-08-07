93897cb0b99336dbfbf8a8f245fee7f5ab4b3971
joseph townshend <joetownshend@mac.com>
2019-08-07 20:03:37 +0100

    FEAT: returning Fizz when 3, with passing test

--- PYTHON/fizzbuzz.py
+++ PYTHON/fizzbuzz.py
@@ -5,5 +5,7 @@ class FizzBuzz(object):
     def fizzbuzzer(self):
         if (self.text % 15) == 0:
             return("FizzBuzz")
+        elif (self.text % 3) == 0:
+            return("Fizz")   
         else:
             return(self.text)
--- PYTHON/fizzbuzz_test.py
+++ PYTHON/fizzbuzz_test.py
@@ -11,3 +11,8 @@ class TestFizzBuzz(unittest.TestCase):
         from fizzbuzz import FizzBuzz
         fizzbuzz = FizzBuzz(15)
         self.assertEqual(fizzbuzz.fizzbuzzer(), "FizzBuzz")
+
+    def test_method_returns_fizz_when_3(self):
+        from fizzbuzz import FizzBuzz
+        fizzbuzz = FizzBuzz(3)
+        self.assertEqual(fizzbuzz.fizzbuzzer(), "Fizz")
