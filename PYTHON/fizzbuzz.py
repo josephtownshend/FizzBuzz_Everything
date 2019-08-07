86c2347d5a341805eb7ef203ebbb91dfc333f3d5
joseph townshend <joetownshend@mac.com>
2019-08-07 19:57:22 +0100

    TEST: returns number 1 with passing test

--- PYTHON/fizzbuzz.py
+++ PYTHON/fizzbuzz.py
@@ -1,4 +1,6 @@
-class Fizzbuzz(object):
+class FizzBuzz(object):
+    def __init__(self, text):
+        self.text = text
 
     def fizzbuzzer(self):
         return(1)
--- PYTHON/fizzbuzz_test.py
+++ PYTHON/fizzbuzz_test.py
@@ -3,4 +3,6 @@ import unittest
 class TestFizzBuzz(unittest.TestCase):
 
     def test_method_returns_number_1(self):
-        from fizzbuzz import Fizzbuzz
+        from fizzbuzz import FizzBuzz
+        fizzbuzz = FizzBuzz(1)
+        self.assertEqual(fizzbuzz.fizzbuzzer(), 1)
