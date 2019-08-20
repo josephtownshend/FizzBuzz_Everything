efe04ace895e4f65be4e412e4039f73a92561a88
joseph townshend <joetownshend@mac.com>
2019-08-20 13:20:11 +0100

    feat: test passing for multiples of 5:

--- JAVA/src/FizzBuzzConverter.java
+++ JAVA/src/FizzBuzzConverter.java
@@ -4,6 +4,10 @@ public class FizzBuzzConverter {
         if(toConvertToFizzBuzz%3==0) {
             return "Fizz";
         }
+        
+        if(toConvertToFizzBuzz%5==0) {
+            return "Buzz";
+        }
 
         return String.valueOf(toConvertToFizzBuzz);
     }
