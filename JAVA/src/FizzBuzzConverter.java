edb0f4f52c2d7db92cc6b70ed5b9fd6bf6c42984
joseph townshend <joetownshend@mac.com>
2019-08-20 13:22:21 +0100

    feat: tests pass multiples of 15

--- JAVA/src/FizzBuzzConverter.java
+++ JAVA/src/FizzBuzzConverter.java
@@ -1,10 +1,14 @@
 public class FizzBuzzConverter {
     public String convert(int toConvertToFizzBuzz) {
 
+        if(toConvertToFizzBuzz%15==0) {
+            return "FizzBuzz";
+        }
+
         if(toConvertToFizzBuzz%3==0) {
             return "Fizz";
         }
-        
+
         if(toConvertToFizzBuzz%5==0) {
             return "Buzz";
         }
