27852aca53847d554547d7c45ddf1fbb79263fe8
joseph townshend <joetownshend@mac.com>
2019-08-20 13:00:56 +0100

    feat: java start from scratch

--- JAVA/src/FizzBuzz.java
+++ JAVA/src/FizzBuzz.java
@@ -1,5 +1,3 @@
 public class FizzBuzz {
-    public String fizzBuzzerMethod(String one, String two){
-        return one + two;
-    }
+  
 }
--- JAVA/src/test/FizzBuzzTest.java
+++ JAVA/src/test/FizzBuzzTest.java
@@ -3,13 +3,4 @@ import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
 
-    @Test
-    public void fizzBuzzerMethod() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        String result = fizzBuzz.fizzBuzzerMethod("one", "two");
-
-        assertEquals("onetwo", result);
-
-    }
 }
