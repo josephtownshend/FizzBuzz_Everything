2943ea25a79f69dcbbecff598d8a7663a86ffc17
joseph townshend <joetownshend@mac.com>
2019-08-20 13:11:43 +0100

    test: failing test for returning normal nums

--- JAVA/src/test/FizzBuzzTest.java
+++ JAVA/src/test/FizzBuzzTest.java
@@ -1,6 +1,14 @@
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
 
+    @Test
+    public void FizzBuzzConverterLeavesNormalNumbersAlone() {
+
+        FizzBuzzConverter fizzBuzz = new FizzBuzzConverter();
+
+        Assert.assertEquals("1", fizzBuzz.convert(1));
+    }
 }
