bdd38cd7c48920145109e666d272245ae95ceb94
joseph townshend <joetownshend@mac.com>
2019-08-31 12:58:59 +0100

    feat: can return Fizz for multiples of 3

--- RUST/fizzbuzz/src/main.rs
+++ RUST/fizzbuzz/src/main.rs
@@ -1,5 +1,9 @@
 pub fn fizzbuzz(x: usize) -> String {
+    if x == 3 {
+        return String::from("Fizz");
+    } else {
     return x.to_string();
+    }
 }
 
 #[cfg(test)]
@@ -8,6 +12,17 @@ mod tests {
 
     #[test]
     fn test_fizzbuzz_1() {
-        assert_eq!(fizzbuzz(1), "1");    // Pass
+        assert_eq!(fizzbuzz(1), "1");
     }
+
+    #[test]
+    fn test_fizzbuzz_2() {
+        assert_eq!(fizzbuzz(2), "2");
+    }
+
+    #[test]
+    fn test_fizzbuzz_3() {
+        assert_eq!(fizzbuzz(3), "Fizz");
+    }
+
 }
