a39341a449c00dc81e183a663ca127756cc160fc
joseph townshend <joetownshend@mac.com>
2019-08-31 12:56:35 +0100

    feat: can return nums that return themselves

--- RUST/fizzbuzz/src/main.rs
+++ RUST/fizzbuzz/src/main.rs
@@ -1,8 +1,13 @@
-fn main() {
-    println!("FizzBuzz");
+pub fn fizzbuzz(x: usize) -> String {
+    return x.to_string();
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
     #[test]
-    fn it_works() {
+    fn test_fizzbuzz_1() {
+        assert_eq!(fizzbuzz(1), "1");    // Pass
     }
+}
