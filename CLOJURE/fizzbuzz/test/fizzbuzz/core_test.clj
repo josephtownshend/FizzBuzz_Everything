b9ac674e229791775c9efbddc92c0a68cc24db9f
joseph townshend <joetownshend@mac.com>
2019-08-31 10:49:46 +0100

    test: extended fizz test

--- CLOJURE/fizzbuzz/test/fizzbuzz/core_test.clj
+++ CLOJURE/fizzbuzz/test/fizzbuzz/core_test.clj
@@ -8,4 +8,5 @@
     (is (= 1 (fizzbuzz 1))))
 
   (testing "fizzbuzz with numbers that return 'Fizz'"
-    (is (= "Fizz" (fizzbuzz 3)))))
+    (is (= "Fizz" (fizzbuzz 3)))
+    (is (= "Fizz" (fizzbuzz 6)))))
