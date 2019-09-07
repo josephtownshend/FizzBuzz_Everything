e074ba97098e9b05948260c5a626d95d57ab55a2
joseph townshend <joetownshend@mac.com>
2019-09-07 18:34:16 +0100

    setup: swift

---
+++ C#/FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class FizzBuzzer
+    {
+        public string FIzzBuzz(int input)
+
+        {
+            return "1";
+        }
+    }
+}
