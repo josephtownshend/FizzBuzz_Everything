529ba633e9ba4bf9d033460c60f99f20e25314f9
joseph townshend <joetownshend@mac.com>
2019-09-09 09:17:34 +0100

    refatcor: tests to use let brain

--- SWIFT/FizzBuzz/FizzBuzz/Brain.swift
+++ SWIFT/FizzBuzz/FizzBuzz/Brain.swift
@@ -1,5 +1,14 @@
 class Brain {
+    
+    func isDivisibleBy(divisor: Int, number: Int) -> Bool {
+        return number % divisor == 0
+    }
     func isDivisibleByThree(number: Int) -> Bool {
-        return true
+        return isDivisibleBy(divisor: 3, number: number)
+    }
+    func isDivisibleByFive(number: Int) -> Bool {
+        return isDivisibleBy(divisor: 5, number: number)
     }
 }
+
+
--- SWIFT/FizzBuzz/FizzBuzzTests/BrainTests.swift
+++ SWIFT/FizzBuzz/FizzBuzzTests/BrainTests.swift
@@ -1,14 +1,23 @@
 import XCTest
 @testable import FizzBuzz
+ let brain = Brain()
 
 func testIsDivisibleByThree() {
-    let brain = Brain()
     let result = brain.isDivisibleByThree(number: 3)
     XCTAssertEqual(result, true)
 }
 
 func testIsNotDivisibleByThree() {
-    let brain = Brain()
     let result = brain.isDivisibleByThree(number: 1)
+    XCTAssertEqual(result, false)
+}
+
+func testIsDivisibleByFive() {
+    let result = brain.isDivisibleByFive(number: 5)
+    XCTAssertEqual(result, true)
+}
+
+func testIsNotDivisibleByFive() {
+    let result = brain.isDivisibleByFive(number: 1)
     XCTAssertEqual(result, false)
 }
