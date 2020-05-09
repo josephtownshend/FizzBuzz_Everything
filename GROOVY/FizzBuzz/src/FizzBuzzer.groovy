ac3b96be61fe61d25802234713ad7b07ed8f9710
Joseph Townshend <joetownshend@mac.com>
2020-05-09 15:57:04 +0100

    passing condition for FizzBuzz

--- GROOVY/FizzBuzz/src/FizzBuzzer.groovy
+++ GROOVY/FizzBuzz/src/FizzBuzzer.groovy
@@ -2,7 +2,9 @@
 class FizzBuzzer {
 
   def static fizzBuzz(input) {
-    if (input % 3 == 0) {
+    if (input % 15 == 0) {
+      return "FizzBuzz"
+    } else if (input % 3 == 0) {
       return "Fizz"
     } else if (input % 5 == 0) {
       return "Buzz"
