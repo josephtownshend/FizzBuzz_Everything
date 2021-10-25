735fd7aaf14e317c58770316166c35852360a828
joseph townshend <joetownshend@mac.com>
2021-10-25 17:14:17 +0100

    failing test for multiples of 5 and bolster 3

--- CRYSTAL/fizzbuzz/test/fizzbuzz_test.cr
+++ CRYSTAL/fizzbuzz/test/fizzbuzz_test.cr
@@ -7,8 +7,15 @@ class LearningTest < Minitest::Test
     @fizzbuzz ||= LearningMinitest::Fizzbuzz.new
   end
 
-  def test_fizzbuzz
+  def test_fizzbuzz_multiples_of_3
     assert_equal "Fizz", fizzbuzz.fizzbuzzer(3)
+    assert_equal "Fizz", fizzbuzz.fizzbuzzer(6)
+    assert_equal "Fizz", fizzbuzz.fizzbuzzer(9000)
+    assert_equal "Fizz", fizzbuzz.fizzbuzzer(666)
+  end
+
+  def test_fizzbuzz_multiples_of_5
+    assert_equal "Buzz", fizzbuzz.fizzbuzzer(5)
   end
 
 end
